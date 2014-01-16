8183c7ce1332759d67dba9c1549ce9c3ed33e23b
Alexander Dean <dstar@csh.rit.edu>
2014-01-16 03:24:28 -0500

    Pushing old alterations to make way for refactoring.

--- src/erlam_compile.erl
+++ src/erlam_compile.erl
@@ -8,7 +8,7 @@
 
 
 compile_file( Path ) -> compile_file( Path, [] ).
-compile_file( Path, _Configs ) -> %TODO: Ignoring comfigurations for now.
+compile_file( Path, Configs ) -> %TODO: Ignoring comfigurations for now.
     %TODO: NO ERROR HANDLING?!
     {ok, BinaryContents} = file:read_file( Path ),
     StringContents = erlang:binary_to_list(BinaryContents),
@@ -16,12 +16,12 @@ compile_file( Path, _Configs ) -> %TODO: Ignoring comfigurations for now.
     {ok, ExprTree} = erlam_parser:parse( Tokens ),
     AST = erlam_lib:update( ExprTree ), 
     UserCode = erlam_trans:to_erl(AST),
-    push_code( Path, UserCode ).
+    push_code( getType(Configs), Path, UserCode ).
 
 
-push_code( Path, UserCode ) ->
+push_code( Type, Path, UserCode ) ->
     {ok, BasePath, ModName} = get_path_info( Path ),
-    {ok, FilePath, _Source} = build_user_module( BasePath, ModName, UserCode ),
+    {ok, FilePath, _Source} = build_user_module( Type, BasePath, ModName, UserCode ),
     build_executable( FilePath ).
 
 get_path_info( Path ) ->
@@ -32,23 +32,41 @@ strip_ext( Path ) -> lists:takewhile( fun (C) -> C /= $. end, Path ).
 
 % Creates the file ModName.erl where ModName.els was the source file
 % used when called compile_file/1.
-build_user_module( BasePath, ModName, UserCode ) ->
+build_user_module( Type, BasePath, ModName, UserCode ) ->
     FilePath = string:join([BasePath,ModName],"/"),
-    Source = wrap_user_code(ModName, UserCode),
-    file:write_file(string:concat(FilePath,".erl"),Source),
+    Source = wrap_user_code(Type, ModName, UserCode),
+    case Type of erlang ->
+                    file:write_file(string:concat(FilePath,".erl"),Source);
+                 escript ->
+                    file:write_file(FilePath,Source)
+    end,            
     {ok, FilePath, Source}.
         
 % Combines the rts,chan, and UserCode modules together into an archive escript.
 build_executable(_Path) -> ok.
 
-wrap_user_code( Module, Code ) ->
+wrap_user_code( erlang, Module, Code ) ->
     io_lib:format( 
         string:join(["-module(~s).",
                      "-export([main/1]).",
-                     "main(_)->",
-                     "erlam_rts:setup([]),",
+                     "main(Args) when is_list(Args)->",
+                     "erlam_rts:setup(Args),",
                      "X = ~s,",
                      "erlam_rts:breakdown(),",
                      "X."],"~n"),
-                  [Module,Code]).
+                  [Module,Code]);
+
+wrap_user_code( escript, Module, Code ) ->
+    io_lib:format(
+        string:join(["#!/usr/bin/env escript",
+                     "%% -*- erlang -*-",
+                     "%%! -smp enable -sname ~s",
+                     "main(Args) ->",
+                     "erlam_rts:setup(Args),",
+                     "X = ~s,",
+                     "erlam_rts:breakdown(),",
+                     "X."],"~n"),
+        [Module, Code]).
+
+getType( _Configs ) -> escript.
 
